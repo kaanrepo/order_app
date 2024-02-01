6d9fa51e869fc003faf048ecd96a0fe015639d76
kaanrepo <gkaanatsever@gmail.com>
2024-02-01 21:59:48 +0300

    Model changes

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.utils.text import slugify
+import random
+import string
 
 UNIT_CHOICES = [
     ('draft', 'Draft'),
@@ -15,46 +17,75 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     unit = models.CharField(max_length=50, choices=UNIT_CHOICES ,default='draft')
     size = models.CharField(max_length=50)
+    handle = models.SlugField(null=True, blank=True)
     image = models.ImageField(upload_to='product', blank=True, null=True)
     image2 = models.ImageField(upload_to='product', blank=True, null=True)
+
+    class Meta:
+        unique_together = ('name', 'unit', 'size',)
 
     def __str__(self):
         return self.name + ' ' + self.size + ' ' + self.unit
     
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            slug_string = '-'.join([self.name, self.size, self.unit])
+            self.handle = slugify(slug_string)
+        super().save(*args, **kwargs)
+
 class MenuCategory(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+    handle = models.SlugField(null=True, blank=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            self.handle = slugify(self.name)
+        super().save(*args, **kwargs)
     
 class MenuItem(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
     category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    handle = models.SlugField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.product.name + ' ' + self.product.size + ' ' + self.product.unit + ' ' + str(self.price)
     
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            slug_string = '-'.join([self.product.name, self.product.size, self.product.unit])
+            self.handle = slugify(slug_string)
+        super().save(*args, **kwargs)
+    
 class Table(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     in_use = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
+    handle = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        if not self.handle:
+            self.handle = slugify(self.name)
+        super().save(*args, **kwargs)
+
 class OrderItem(models.Model):
     order = models.ForeignKey('Order', on_delete=models.CASCADE)
-    product = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_delivered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.product.product.name + 'x' + str(self.quantity)
+        return self.menu_item.product.name + 'x' + str(self.quantity)
     
 class Order(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
