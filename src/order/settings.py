9ab97a95d8a65c2de4e13a48babbeba3188d5fe7
kaanrepo <gkaanatsever@gmail.com>
2024-02-01 19:35:13 +0300

    updated for whitenoise

--- src/order/settings.py
+++ src/order/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +120,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'static-root'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+STORAGES = {
+    'default': {
+        "BACKEND": 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        "BACKEND": STATICFILES_STORAGE,
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
